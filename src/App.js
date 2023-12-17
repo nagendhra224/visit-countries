347caddecfc2815c791d2b4dcaabc6b227927841
nagendhra224 <pdattanagendhra8@gmail.com>
2023-12-17 09:17:14 +0000

    app

---
+++ src/App.js
@@ -0,0 +1,189 @@
+import {Component} from 'react'
+import './App.css'
+
+const initialCountriesList = [
+  {
+    id: '53c9c67a-c923-4927-8a75-fdfc4bc5ec61',
+    name: 'Australia',
+    imageUrl:
+      'https://assets.ccbp.in/frontend/react-js/visit-countries-australia-img.png',
+    isVisited: false,
+  },
+  {
+    id: '8baa8029-fb2c-4f06-bfcc-3dc9ad12b24d',
+    name: 'Canada',
+    imageUrl:
+      'https://assets.ccbp.in/frontend/react-js/visit-countries-canada-img.png',
+    isVisited: false,
+  },
+  {
+    id: '1b520f98-6548-41f3-816e-c8b887865172',
+    name: 'Greenland',
+    imageUrl:
+      'https://assets.ccbp.in/frontend/react-js/visit-countries-greenland-img.png',
+    isVisited: false,
+  },
+  {
+    id: '25841996-fbfd-4554-add4-4c94082c8ccd',
+    name: 'India',
+    imageUrl:
+      'https://assets.ccbp.in/frontend/react-js/visit-countries-india-img.png',
+    isVisited: true,
+  },
+  {
+    id: '603c3568-13b0-11ec-82a8-0242ac130003',
+    name: 'Netherlands',
+    imageUrl:
+      'https://assets.ccbp.in/frontend/react-js/visit-countries-netherland-img.png',
+    isVisited: false,
+  },
+  {
+    id: '3c988dec-55e1-477d-a9e2-b354fd559849',
+    name: 'Portugal',
+    imageUrl:
+      'https://assets.ccbp.in/frontend/react-js/visit-countries-portugal-img.png',
+    isVisited: false,
+  },
+  {
+    id: 'd766f754-34f7-413e-81ec-9992821b97fa',
+    name: 'Switzerland',
+    imageUrl:
+      'https://assets.ccbp.in/frontend/react-js/visit-countries-switzerland-img.png',
+    isVisited: false,
+  },
+  {
+    id: '7ebb4e04-b124-417f-a69e-564a456d70f1',
+    name: 'Thailand',
+    imageUrl:
+      'https://assets.ccbp.in/frontend/react-js/visit-countries-thailand-img.png',
+    isVisited: false,
+  },
+  {
+    id: '1e4b1dcd-6ace-4dde-ad8d-675927d5ae47',
+    name: 'United Kingdom',
+    imageUrl:
+      'https://assets.ccbp.in/frontend/react-js/visit-countries-united-kingdom-img.png',
+    isVisited: true,
+  },
+  {
+    id: 'e76da8ca-bc48-4981-902b-a4d2d46feb6d',
+    name: 'Venezuela',
+    imageUrl:
+      'https://assets.ccbp.in/frontend/react-js/visit-countries-venezuela-img.png',
+    isVisited: false,
+  },
+  {
+    id: '8baa8029-fb2c-4f06-bfcc-3dc9ad12b24d',
+    name: 'Canada',
+    imageUrl:
+      'https://assets.ccbp.in/frontend/react-js/visit-countries-canada-img.png',
+    isVisited: false,
+  },
+  {
+    id: '7ebb4e04-b124-417f-a69e-564a456d70f6',
+    name: 'Thailand',
+    imageUrl:
+      'https://assets.ccbp.in/frontend/react-js/visit-countries-thailand-img.png',
+    isVisited: false,
+  },
+]
+
+class App extends Component {
+  state = {
+    countries: initialCountriesList,
+    visitedCountries: initialCountriesList.filter(country => country.isVisited),
+  }
+
+  onClickVisit = country => {
+    const {countries, visitedCountries} = this.state
+
+    const updatedCountries = countries.map(eachCountry =>
+      eachCountry.id === country.id
+        ? {...eachCountry, isVisited: true}
+        : eachCountry,
+    )
+
+    const updatedVisitedCountries = [...visitedCountries, country]
+
+    this.setState({
+      countries: updatedCountries,
+      visitedCountries: updatedVisitedCountries,
+    })
+  }
+
+  onClickRemove = visitedCountry => {
+    const {countries, visitedCountries} = this.state
+
+    const updatedVisitedCountries = visitedCountries.filter(
+      eachCountry => eachCountry.id !== visitedCountry.id,
+    )
+
+    const updatedCountries = countries.map(eachCountry =>
+      eachCountry.id === visitedCountry.id
+        ? {...eachCountry, isVisited: false}
+        : eachCountry,
+    )
+
+    this.setState({
+      countries: updatedCountries,
+      visitedCountries: updatedVisitedCountries,
+    })
+  }
+
+  render() {
+    const {countries, visitedCountries} = this.state
+
+    return (
+      <div className="app-container">
+        <h1 className="heading">Countries</h1>
+        <ul className="country-list">
+          {countries.map(country => (
+            <li key={country.id} className="country country-container">
+              <p className="name">{country.name}</p>
+              {country.isVisited ? (
+                <p>Visited</p>
+              ) : (
+                <button
+                  type="button"
+                  className="button"
+                  onClick={() => this.onClickVisit(country)}
+                >
+                  Visit
+                </button>
+              )}
+            </li>
+          ))}
+        </ul>
+        <div>
+          <h1 className="heading">Visited Countries</h1>
+          <ul className="countries-image-container">
+            {visitedCountries.map(visitedCountry => (
+              <li key={visitedCountry.id} className="country-image-list">
+                <img
+                  src={visitedCountry.imageUrl}
+                  alt="thumbnail"
+                  className="image"
+                />
+                <div className="name-remove-container">
+                  <p>{visitedCountry.name}</p>
+                  <button
+                    type="button"
+                    className="remove-button"
+                    onClick={() => this.onClickRemove(visitedCountry)}
+                  >
+                    Remove
+                  </button>
+                </div>
+              </li>
+            ))}
+          </ul>
+          {visitedCountries.length === 0 ? (
+            <p className="no-countries-visited">No Countries Visited Yet</p>
+          ) : null}
+        </div>
+      </div>
+    )
+  }
+}
+
+export default App
